4c58405b348646ea8f4240c09bc2b72c0d41230d
AlexP951 <alexander.c.pearle@vanderbilt.edu>
2025-12-04 15:58:20 -0600

    Update tests.go

--- tests.go
+++ tests.go
@@ -1,43 +1,49 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestPuzzle1(t *testing.T) {
-	grid, _ := ReadPuzzle("txt/sudoku-test1.txt")
-	expected, _ := ReadPuzzle("txt/sudoku-test1-solved.txt")
+	grid := readFileIntoGrid("txt/sudoku-test1.txt")
+	expected := readFileIntoGrid("txt/sudoku-test1-solved.txt")
 
 	Solve(grid)
 
 	for r := 0; r < 9; r++ {
 		for c := 0; c < 9; c++ {
 			if grid[r][c] != expected[r][c] {
-				t.Errorf("Mismatch at (%d,%d): got %d, expected %d",
+				t.Errorf("Incorrect Solver",
 					r, c, grid[r][c], expected[r][c])
 			}
 		}
 	}
+	fmt.Println("Puzzle 1 solved correctly")
 }
 
 func TestPuzzle2(t *testing.T) {
-	grid, _ := ReadPuzzle("txt/sudoku-test2.txt")
-	expected, _ := ReadPuzzle("txt/sudoku-test2-solved.txt")
+	grid := readFileIntoGrid("txt/sudoku-test2.txt")
+	expected := readFileIntoGrid("txt/sudoku-test2-solved.txt")
 
 	Solve(grid)
 
 	for r := 0; r < 9; r++ {
 		for c := 0; c < 9; c++ {
 			if grid[r][c] != expected[r][c] {
-				t.Errorf("Mismatch at (%d,%d): got %d, expected %d",
+				t.Errorf("Incorrect Solver",
 					r, c, grid[r][c], expected[r][c])
 			}
 		}
 	}
+	fmt.Println("Puzzle 2 solved correctly")
 }
 
 func TestImpossible(t *testing.T) {
-	grid, _ := ReadPuzzle("txt/sudoku-impossible.txt")
+	grid := readFileIntoGrid("txt/sudoku-impossible.txt")
 
 	if Solve(grid) {
-		t.Errorf("Impossible puzzle incorrectly solved!")
+		t.Errorf("Impossible puzzle incorrectly solved")
 	}
+	fmt.Println("Impossible puzzle did not have a solution which is correct")
 }
